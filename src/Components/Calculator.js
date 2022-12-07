eceba8f75b01f1711f46002872c77c22f16e0494
SekoVper <damehlivingstone1@gmail.com>
2022-12-07 16:35:46 +0100

    Add event handling for calculator keys

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,45 +1,53 @@
 /* eslint-disable no-plusplus */
-
 import React from 'react';
+import calculate from '../Logic/calculate';
 
-const createDigits = () => {
-  const digits = [];
-  for (let i = 1; i < 10; i++) {
-    digits.push(
-      <button type="button" key={i}>{i}</button>,
-    );
-  }
-  return digits;
-};
-
-class Calculator extends React.Component {
-  constructor() {
-    super();
+export default class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
     this.state = {};
+    this.buttonClick = this.buttonClick.bind(this);
+  }
+
+  createDigits = () => {
+    const digits = [];
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button type="button" key={i} onClick={this.buttonClick}>{i}</button>,
+      );
+    }
+    return digits;
+  };
+
+  buttonClick(element) {
+    this.setState((state) => calculate(state, element.target.textContent));
   }
 
   render() {
+    const { next } = this.state;
+    const { total } = this.state;
     return (
       <div className="calculator">
         <div className="display">
-          <span className="result">0</span>
+          <span className="results">{next || total || 0}</span>
         </div>
 
         <div className="buttons">
           <div className="digits">
-            <button type="button">AC</button>
-            <button type="button">+/-</button>
-            <button type="button">%</button>
-            {createDigits()}
-            <button className="span-row" type="button">0</button>
-            <button className="s" type="button">.</button>
+            <button type="button" onClick={this.buttonClick}>AC</button>
+            <button type="button" onClick={this.buttonClick}>+/-</button>
+            <button type="button" onClick={this.buttonClick}>%</button>
+            {this.createDigits()}
+            <button className="span-row" type="button" onClick={this.buttonClick}>0</button>
+            <button className="s" type="button" onClick={this.buttonClick}>.</button>
           </div>
+
           <div className="operators">
-            <button className="operator" type="button">/</button>
-            <button className="operator" type="button">+</button>
-            <button className="operator" type="button">-</button>
-            <button className="operator" type="button">*</button>
-            <button className="operator" type="button">=</button>
+            <button className="operator" type="button" onClick={this.buttonClick}>/</button>
+            <button className="operator" type="button" onClick={this.buttonClick}>+</button>
+            <button className="operator" type="button" onClick={this.buttonClick}>-</button>
+            <button className="operator" type="button" onClick={this.buttonClick}>x</button>
+            <button className="operator" type="button" onClick={this.buttonClick}>=</button>
           </div>
         </div>
       </div>
@@ -47,4 +55,4 @@ class Calculator extends React.Component {
   }
 }
 
-export default Calculator;
+// export default Calculator;
